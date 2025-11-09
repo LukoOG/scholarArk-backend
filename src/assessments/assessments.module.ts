01319e395cf9f8067eaaa5e75b37462788b59db8
unknown <emmanueladesipe01@gmail.com>
2025-11-09 23:26:56 +0100

    resolved UserGuard DI issue

--- src/assessments/assessments.module.ts
+++ src/assessments/assessments.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AssessmentsService } from './assessments.service';
 import { AssessmentsController } from './assessments.controller';
 import { Assessment, AssessmentSchema } from './schemas/assessments.schema';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Assessment.name, schema: AssessmentSchema },
     ]),
+	UserModule
   ],
   controllers: [AssessmentsController],
   providers: [AssessmentsService],
