3d49dc602c7d716c5ae7c7ee6f67338b1f1b4363
unknown <emmanueladesipe01@gmail.com>
2025-12-17 17:48:06 +0100

    fixed build errors

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserFcmToken, UserFcmTokenSchema } from './schemas/user-fcm-token.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: UserFcmToken.name, schema: UserFcmTokenSchema }])],
+  imports: [
+	MongooseModule.forFeature([{ name: UserFcmToken.name, schema: UserFcmTokenSchema }]),
+	AuthModule
+  ],
   providers: [NotificationsService],
   controllers: [NotificationsController],
 })
