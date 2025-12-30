633f33583b8a92afa40360e2f3c345213efc9ffc
unknown <emmanueladesipe01@gmail.com>
2025-12-30 06:25:14 +0100

    cached meta endpoints

--- src/meta/meta.controller.ts
+++ src/meta/meta.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, } from '@nestjs/swagger';
 import { TopicService } from '../topics/topics.service';
 import { GoalService } from '../goals/goals.service';
 import { PreferenceService } from '../preferences/preferences.service';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
 @ApiTags('Meta')
 @Controller('meta')
+@UseInterceptors(CacheInterceptor)
 export class MetaController {
 	constructor(
 		private readonly topicService: TopicService,
@@ -18,6 +20,8 @@ export class MetaController {
 	  description: 'Used during user registration to select learning topics',
 	})
 	@ApiResponse({ status: 200, description: 'List of topics' })
+	@CacheKey('meta:topics')
+	@CacheTTL(60 * 60)
 	@Get('topics')
 	async getTopics(){
 		return this.topicService.findActive()
@@ -28,6 +32,8 @@ export class MetaController {
 	  description: 'Used during user registration to select learning goals',
 	})
 	@ApiResponse({ status: 200, description: 'List of topics' })
+	@CacheKey('meta:goals')
+	@CacheTTL(60 * 60)
 	@Get('goals')
 	async getGoals(){
 		return this.goalService.findActive()
@@ -38,6 +44,8 @@ export class MetaController {
 	  description: 'Used during user registration to select learning preferences',
 	})
 	@ApiResponse({ status: 200, description: 'List of topics' })
+	@CacheKey('meta:preferences')
+	@CacheTTL(60 * 60)
 	@Get('preferences')
 	async getPreferences(){
 		return this.preferenceService.findActive()
